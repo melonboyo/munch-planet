598ebafafa48ed055da9ea60bb07f3c62cb8a76d
Jonas Lund <jonas.chuck.lund@gmail.com>
2023-11-30 19:52:00 +0100

    Grass shell texture progress

---
+++ addons/shell_fur/shell_fur_physics.gd
@@ -0,0 +1,44 @@
+# Copyright © 2021 Kasper Arnklit Frandsen - MIT License
+# See `LICENSE.md` included in the source distribution for details.
+const ShellFurManager = preload("res://addons/shell_fur/shell_fur_manager.gd")
+
+var _shell_fur_object : ShellFurManager
+var _trans_momentum : Vector3
+var _rot_momentum : Vector3
+var _physics_pos : Vector3
+var _physics_rot : Quaternion
+
+
+func init(shell_fur_object : ShellFurManager) -> void:
+	_shell_fur_object = shell_fur_object
+
+
+func process(delta) -> void:
+	var position_diff := _current_physics_object().global_transform.origin - _physics_pos
+	_trans_momentum += position_diff * _shell_fur_object.physics_spring
+	_trans_momentum += Vector3(0.0, -1.0 * _shell_fur_object.physics_gravity, 0.0)
+	_physics_pos += _trans_momentum * delta
+	_trans_momentum *= _shell_fur_object.physics_damping * -1 + 1
+	
+	_shell_fur_object.set_shader_param("i_physics_pos_offset", -position_diff)
+	
+	var rot_diff := _physics_rot.inverse() * _current_physics_object().global_transform.basis.get_rotation_quaternion()
+	_rot_momentum += rot_diff.get_euler() * _shell_fur_object.physics_spring
+	_physics_rot *= Quaternion(Vector3.FORWARD, _rot_momentum * delta)
+	_physics_rot = _physics_rot.normalized()
+	_rot_momentum *= _shell_fur_object.physics_damping * -1 + 1
+	
+	_shell_fur_object.set_shader_param("i_physics_rot_offset", rot_diff)
+
+
+func _current_physics_object() -> Node3D:
+	if _shell_fur_object.physics_custom_physics_pivot.is_empty():
+		return _shell_fur_object
+	else:
+		return _shell_fur_object.get_node(_shell_fur_object.physics_custom_physics_pivot) as Node3D
+
+
+func update_physics_object(delay : float) -> void:
+	await _shell_fur_object.get_tree().create_timer(delay).timeout
+	_physics_pos = _current_physics_object().global_transform.origin
+	_physics_rot = _current_physics_object().global_transform.basis.get_rotation_quaternion().normalized()
