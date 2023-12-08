4b1df8088d1de0b8cd4f577260858fbc656e05bc
Per Christian Kvalvik <pc@kvalvik.no>
2023-12-08 23:05:16 +0100

    Remove groups in scene

--- Globals/Cutscenes/CutsceneScene.gd
+++ Globals/Cutscenes/CutsceneScene.gd
@@ -1,8 +1,5 @@
 extends Resource
 class_name CutsceneScene
 
-@export_group("Animation", "animation_")
 @export var animation_name := ""
-
-@export_group("Dialogue", "dialogue_")
 @export var dialogue_translation_key := ""
